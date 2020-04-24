f36fd51caa7281f7b65a400465da467464944f7c
leovidalgithub <leo@leovidal.es>
2020-04-24 05:38:17 -0400

    April 24 2020

---
+++ 1811229_Leonardo_Rodriguez_Project/app/src/main/java/com/example/a1811229_leonardo_rodriguez_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a1811229_leonardo_rodriguez_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
